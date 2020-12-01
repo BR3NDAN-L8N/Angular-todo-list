e8b3c50f3247cf88601b7d289ef1ccd38f4fcf11
Brendan Leighton <BR3NDAN.L8N@gmail.com>
2020-12-01 15:11:59 -0500

    moved static todos to a service

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -10,11 +10,30 @@ export class TodoItemComponent implements OnInit {
 
     // @Input inputs the "todo" into the todo-item.html
     //  "todo" of type "Todo", "Todo" gives the data, "Todo" is in ../todos/todos..ts
-    @Input() todo: Todo;  //  this "todo" is the [todo]="todosObj" in todos.html
+    @Input() todo: Todo;  //  this "todo" is the [todo]="todosObj" in todos..html
 
     constructor() { }
 
     ngOnInit(): void {
     }
 
+    //  SET DYNAMIC CLASSES in todo-item..html
+    setClasses() {
+        let classes = {
+            todo: true,
+            'is-complete': this.todo.completed
+        }
+        return classes;
+    }
+
+    onToggle(todo) {
+        console.log('toggle');
+        todo.completed = !todo.completed;
+
+    }
+
+    onDelete(todo) {
+        console.log('delete');
+
+    }
 }
--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from "../../models/Todo";
+import { TodoService } from "../../services/todo.service";
 
 @Component({
     selector: 'app-todos',
@@ -11,26 +12,10 @@ export class TodosComponent implements OnInit {
     todos: Todo[];
 
     // constructor is used to import services
-    constructor() { }
+    constructor(private todoService: TodoService) { }
 
-    ngOnInit(): void {
-        this.todos = [
-            {
-                id: 1,
-                title: 'Todo One',
-                completed: false
-            },
-            {
-                id: 2,
-                title: 'Todo Two',
-                completed: true
-            },
-            {
-                id: 3,
-                title: 'Todo Three',
-                completed: false
-            }
-        ]
+    ngOnInit() {
+        this.todos = this.todoService.getTodos();
     }
 
 }
